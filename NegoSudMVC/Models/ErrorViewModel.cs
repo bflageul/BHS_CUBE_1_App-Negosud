6638e48dd3e85bf377deb937f44d65bf44a9f7bc
bflageul <bflageul.pro@gmail.com>
2021-10-24 22:17:14 +0200

    add first NegoSudMVC application

---
+++ NegoSudMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NegoSudMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
