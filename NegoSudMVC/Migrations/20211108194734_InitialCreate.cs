5160ee8f9ddb48eda9c186bee4dfb05ecb7a627f
bflageul <bflageul.pro@gmail.com>
2021-11-08 22:24:02 +0100

    connection to local database OK. able to READ UPDATE DELETE Supplier table from web page

---
+++ NegoSudMVC/Migrations/20211108194734_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NegoSudMVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Supplier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address_Id = table.Column<int>(nullable: false),
+                    SocialReason = table.Column<string>(nullable: true),
+                    SIRET = table.Column<string>(nullable: true),
+                    APE_NAF = table.Column<string>(nullable: true),
+                    TVA = table.Column<string>(nullable: true),
+                    Manager = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Supplier", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Supplier");
+        }
+    }
+}
