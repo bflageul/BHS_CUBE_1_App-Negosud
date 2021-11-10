ce4b29374d55b16e24c695c0814e3b5ffc925c65
bflageul <bflageul.pro@gmail.com>
2021-11-10 11:49:42 +0100

    create new MVC solution, update to .NET5.0, first scaffold to Models, connection to local dB

---
+++ NegosudApp/NegosudApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NegosudApp.Models
+{
+    public partial class Employee
+    {
+        public int UsersId { get; set; }
+        public string Position { get; set; }
+
+        public virtual User Users { get; set; }
+    }
+}
