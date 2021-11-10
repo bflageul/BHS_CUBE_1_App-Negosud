26a8509290995748e48016d665f7358280824b9d
bflageul <bflageul.pro@gmail.com>
2021-11-10 11:49:42 +0100

    create new MVC solution, update to .NET5.0, first scaffold to Models, connection to local dB

---
+++ NegosudApp/NegosudApp/Models/Alcohol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NegosudApp.Models
+{
+    public partial class Alcohol
+    {
+        public Alcohol()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Range { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
